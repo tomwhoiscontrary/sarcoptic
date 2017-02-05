261c663261a8ff745430531d8bb8f0795b01c209
Tom Anderson <twic@urchin.earth.li>
2017-02-05 01:14:59 +0000

    Remove obsolete stack size information

--- src/main/java/li/earth/urchin/twic/sarcoptic/Kind.java
+++ src/main/java/li/earth/urchin/twic/sarcoptic/Kind.java
@@ -3,15 +3,15 @@ package li.earth.urchin.twic.sarcoptic;
 import org.objectweb.asm.Opcodes;
 
 enum Kind {
-    BOOLEAN(boolean.class, 'Z', Opcodes.IRETURN, Opcodes.ICONST_0, 1),
-    BYTE(byte.class, 'B', Opcodes.IRETURN, Opcodes.ICONST_0, 1),
-    SHORT(short.class, 'S', Opcodes.IRETURN, Opcodes.ICONST_0, 1),
-    INT(int.class, 'I', Opcodes.IRETURN, Opcodes.ICONST_0, 1),
-    LONG(long.class, 'J', Opcodes.LRETURN, Opcodes.LCONST_0, 2),
-    CHAR(char.class, 'C', Opcodes.IRETURN, Opcodes.ICONST_0, 1),
-    FLOAT(float.class, 'F', Opcodes.FRETURN, Opcodes.FCONST_0, 1),
-    DOUBLE(double.class, 'D', Opcodes.DRETURN, Opcodes.DCONST_0, 2),
-    OBJECT(Object.class, 'L', Opcodes.ARETURN, Opcodes.ACONST_NULL, 1) {
+    BOOLEAN(boolean.class, 'Z', Opcodes.IRETURN, Opcodes.ICONST_0),
+    BYTE(byte.class, 'B', Opcodes.IRETURN, Opcodes.ICONST_0),
+    SHORT(short.class, 'S', Opcodes.IRETURN, Opcodes.ICONST_0),
+    INT(int.class, 'I', Opcodes.IRETURN, Opcodes.ICONST_0),
+    LONG(long.class, 'J', Opcodes.LRETURN, Opcodes.LCONST_0),
+    CHAR(char.class, 'C', Opcodes.IRETURN, Opcodes.ICONST_0),
+    FLOAT(float.class, 'F', Opcodes.FRETURN, Opcodes.FCONST_0),
+    DOUBLE(double.class, 'D', Opcodes.DRETURN, Opcodes.DCONST_0),
+    OBJECT(Object.class, 'L', Opcodes.ARETURN, Opcodes.ACONST_NULL) {
         @Override
         public String descriptor(Class type) {
             return this.descriptorSymbol + ClassFileUtils.binaryName(type) + ";";
@@ -33,14 +33,12 @@ enum Kind {
     protected final char descriptorSymbol;
     public final int returnOpcode;
     public final int zeroOpcode;
-    public final int stackSize;
 
-    Kind(Class<?> type, char descriptorSymbol, int returnOpcode, int zeroOpcode, int stackSize) {
+    Kind(Class<?> type, char descriptorSymbol, int returnOpcode, int zeroOpcode) {
         this.type = type;
         this.descriptorSymbol = descriptorSymbol;
         this.returnOpcode = returnOpcode;
         this.zeroOpcode = zeroOpcode;
-        this.stackSize = stackSize;
     }
 
     public String descriptor(Class type) {
